22e1c5f9f58d6cf737192e396221da4ec7f54c47
Vijayalakshmi0593 <92051694+Vijayalakshmi0593@users.noreply.github.com>
2021-10-20 22:43:26 +0530

    Add files via upload

---
+++ Leafgroundlink.java
@@ -0,0 +1,25 @@
+package week2.day2;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Leafgroundlink {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leafground.com/pages/Link.html");
+		String url = driver.findElement(By.linkText("Find where am supposed to go without clicking me?"))
+				.getAttribute("href");
+		System.out.println(url);
+		driver.findElement(By.xpath("(//a[@href='../home.html'])[3]")).click();
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println("The number of links is " + links.size());
+	}
+}
